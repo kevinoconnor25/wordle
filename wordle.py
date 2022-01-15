5b6bc73bed73100dbbb368574de8ac5a0af6408d
= <kevin_oconnor@taylor.edu>
2022-01-14 23:54:22 -0700

    Cleaned up the comments and output. List of words needs a lot of work

--- wordle.py
+++ wordle.py
@@ -4,7 +4,7 @@
 
 import random
 import sys
-from word_lib import *
+import word_lib
 
 ENDC = '\033[0m'
 RED = '\033[41m'
@@ -14,33 +14,25 @@ GRAY = '\033[100m'
 
 # Display the guess with proper colors based on position of letters
 def display_output(answer, guess):
-    line = "_" * (4 * len(answer) + 1)
-    #print(line)
-
-    '''guess_output = ""
-    for letter in guess:
-        guess_output += " %s " % letter
-    guess_output += "|"
-    print(guess_output)
-
-    print(line)'''
-
-    answer_output = ""
+    answer_output = "\n"
+    blocks = "\n"
     idx = 0
     for letter in guess:
         result = ""
         if letter == answer[idx]:
             result = GREEN + " %s " % letter + ENDC
+            blocks += " " + GREEN + "  " + ENDC
         elif guess[idx] in answer:
             result = YELLOW + " %s " % letter + ENDC
+            blocks += " " + YELLOW + "  " + ENDC
         else:
             result = GRAY + " %s " % letter + ENDC
+            blocks += " " + GRAY + "  " + ENDC
         answer_output += " %s" % result
         idx += 1
-    #answer_output += "|"
     print(answer_output)
 
-    #print(line)
+    return blocks + "\n"
 
 # Get input and validate
 def getInput(answer, words):
@@ -48,8 +40,7 @@ def getInput(answer, words):
     while guess not in words:
         if guess == "?":
             printHelp(answer, words)
-        else:
-            guess = input("Not a valid word. Guess again: ")
+        guess = input("Not a valid word. Guess again: ")
     return guess
 
 # Print a list of all possible answers
@@ -78,10 +69,11 @@ def main():
     # Whether there is a timer. True is "on", false is "off"
     timer = "off"
 
-    words = get_words(num_letters)
+    words = word_lib.get_words(num_letters)
     answer = words[random.randrange(len(words))]
     num_guesses = 0
     guess = ""
+    blocks = ""
     correct = False
 
     print("Starting game with the following parameters:")
@@ -93,14 +85,16 @@ def main():
     # Main loop
     while(num_guesses < max_guesses and correct == False):
         guess = getInput(answer, words)
-        display_output(answer, guess)
+        blocks += display_output(answer, guess)
         if guess == answer:
             correct = True
         num_guesses += 1
 
     if correct == True:
-        print("Wordle %s/%s" % (num_guesses, max_guesses))
+        print("\nWordle %s/%s" % (num_guesses, max_guesses))
     else:
         print("Close! Answer was %s" % answer)
+
+    print(blocks)
 
 main()
